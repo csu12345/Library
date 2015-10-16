9314e01eeaa2aae9169e64e9b31a6ea635fe63c9
csu12345 <aew.kalyan@gmail.com>
2015-10-16 23:19:20 +1100

    Lost Book test case
    Tested

---
+++ UnittestForLostBook.java
@@ -0,0 +1,17 @@
+/*unit test case for lost book */
+package library.tests;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import library.entities.Book;
+import library.interfaces.entities.EBookState;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+
+
+public class BookLoseTest {
+    
+}
